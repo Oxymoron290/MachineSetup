f344c891d3053ad94197310c25f5d37588604665
Oxymoron290 <tiomthysturm6@gmail.com>
2018-10-14 22:15:55 -0500

    can't get nuget package to work. I'll just use the powershell script for now

--- Start.ps1
+++ Start.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
-Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/Oxymoron290/MachineSetup/master/OxymoronDev.1.0.0.nupkg -DisableReboots
+refreshenv
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/Oxymoron290/MachineSetup/master/tools/ChocolateyInstall.ps1 -DisableReboots
