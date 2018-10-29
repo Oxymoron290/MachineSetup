e6678ffd85430a7806f3596abff237d1f7f29e13
Oxymoron290 <timothysturm6@gmail.com>
2018-10-29 17:08:20 -0500

    VisualStudio has a problem, disabeling for now.

--- Full.ps1
+++ Full.ps1
@@ -339,7 +339,7 @@ try{
     
     Write-BoxstarterMessage "Installing WINS stack tools"
     cinst dotnetcore-sdk -y --cacheLocation "C:\temp"
-    cinst visualstudio2017enterprise -y --cacheLocation "C:\temp"
+    #cinst visualstudio2017enterprise -y --cacheLocation "C:\temp"
     cinst resharper -y --cacheLocation "C:\temp"
     cinst nugetpackageexplorer -y --cacheLocation "C:\temp"
     cinst azure-data-studio -y --cacheLocation "C:\temp"
@@ -348,8 +348,8 @@ try{
     cinst dotpeek -y --cacheLocation "C:\temp"
     cinst linqpad -y --cacheLocation "C:\temp"
 
-    Install-ChocolateyVsixPackage xunit http://visualstudiogallery.msdn.microsoft.com/463c5987-f82b-46c8-a97e-b1cde42b9099/file/66837/1/xunit.runner.visualstudio.vsix
-    Install-ChocolateyVsixPackage autowrocktestable http://visualstudiogallery.msdn.microsoft.com/ea3a37c9-1c76-4628-803e-b10a109e7943/file/73131/1/AutoWrockTestable.vsix
+    #Install-ChocolateyVsixPackage xunit http://visualstudiogallery.msdn.microsoft.com/463c5987-f82b-46c8-a97e-b1cde42b9099/file/66837/1/xunit.runner.visualstudio.vsix
+    #Install-ChocolateyVsixPackage autowrocktestable http://visualstudiogallery.msdn.microsoft.com/ea3a37c9-1c76-4628-803e-b10a109e7943/file/73131/1/AutoWrockTestable.vsix
 
     Write-BoxstarterMessage "Installing mobile tools"
     cinst androidstudio -y --cacheLocation "C:\temp"
