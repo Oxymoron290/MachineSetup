a6662b34327c1e27588c707f5ffecbe3490e88fc
Oxymoron290 <tiomthysturm6@gmail.com>
2019-03-05 22:07:23 -0600

    Added docker

--- Full.ps1
+++ Full.ps1
@@ -354,6 +354,8 @@ try{
     cinst dotpeek -y --cacheLocation "C:\temp"
     cinst linqpad -y --cacheLocation "C:\temp"
 
+    cinst docker-desktop -y --cacheLocation "C:\temp"
+
     #Install-ChocolateyVsixPackage xunit http://visualstudiogallery.msdn.microsoft.com/463c5987-f82b-46c8-a97e-b1cde42b9099/file/66837/1/xunit.runner.visualstudio.vsix
     #Install-ChocolateyVsixPackage autowrocktestable http://visualstudiogallery.msdn.microsoft.com/ea3a37c9-1c76-4628-803e-b10a109e7943/file/73131/1/AutoWrockTestable.vsix
 
